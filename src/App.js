d2923c1467d81fcd284e4a2e7e97fb077d2015e8
Arjun Sharma <77678077+arjunsharma1152@users.noreply.github.com>
2023-03-28 17:59:42 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
 import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TextInput from './components/TextInput';
+import KeywordsModal from './components/KeywordsModal';
 
 const App = () => {
+  const [keywords, setKeywords] = useState('');
+  const [isOpen, setIsOpen] = useState('');
+  const [loading, setLoading] = useState('');
+  
+  const extractKeyword = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
 
-  const extractKeyword = (text) => {
-    console.log(text);
+    const res = await fetch('https://api.openai.com/v1/chat/completions',{  
+    method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+        Authorisation: `Bearer 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`
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: 'Extract keywords from this Text. Make the first letter of each word uppercase and separate with commas\n\n'+ text + '',
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8
+      })
+    })
+
+    const json = await res.json();
+
+    const data = json.choices[0].text.trim();
+
+    console.log(data);
+    setKeywords(data);
+    setLoading(false);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
   }
 
   return (
@@ -17,6 +50,7 @@ const App = () => {
   <TextInput extractKeyword={extractKeyword}/>
   <Footer />
   </Container>
+  <KeywordsModal keywords={keywords} loading={loading} isOpen={isOpen} closeModal={closeModal} />
   </Box> 
   )
 }
