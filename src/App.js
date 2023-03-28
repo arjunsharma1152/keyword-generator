cf4bad2d3a3cc724ebee45aeb6b2b9f4eff0dff2
Arjun Sharma <77678077+arjunsharma1152@users.noreply.github.com>
2023-03-28 23:48:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     setLoading(true);
     setIsOpen(true);
 
-    const res = await fetch('https://api.openai.com/v1/chat/completions',{  
+    const res = await fetch('https://api.openai.com/v1/completions',{  
     method: 'POST',
       headers: {
         'Content-Type' : 'application/json',
